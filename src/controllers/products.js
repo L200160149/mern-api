8df6223261465445549842ab73983316408e040f
L200160149 <ums.informatika@gmail.com>
2022-06-29 14:19:06 +0700

    api-plainning

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,10 +1,13 @@
 exports.createProduct = (req, res, next) => {
+  console.log("request: ", req.body);
+  const name = req.body.name;
+  const price = req.body.price;
+
   res.json({
     message: "Create Product Success",
     data: {
-      id: 1,
-      name: "Nutrisari",
-      price: 4000,
+      name: name,
+      price: price,
     },
   });
   next();
