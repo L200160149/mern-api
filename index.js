1c1350b3dd9d1d2c86b8e1619333db85561ff8c6
L200160149 <ums.informatika@gmail.com>
2022-06-30 15:25:12 +0700

    create blog

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const productRoutes = require("./src/routes/products");
+const authRoutes = require("./src/routes/auth")
+const blogRoutes = require("./src/routes/blog")
 
 app.use(bodyParser.json());
 
@@ -16,7 +17,15 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use("/", productRoutes);
-app.use("/v1/", productRoutes);
+app.use("/v1/auth", authRoutes);
+app.use("/v1/blog", blogRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.errorStatus || 500;
+  const message = error.message;
+  const data = error.data;
+
+  res.status(status).json({message: message, data: data})
+})
 
 app.listen(4000);
